fb36aaf97b5d475892bd00764a76c2477008903e
takaya <takaya_ishiyama0810@yahoo.co.jp>
2023-11-24 13:06:43 +0900

    test

--- apps/backend/target/debug/deps/domain-49c4e266b534248d.d
+++ apps/backend/target/debug/deps/domain-49c4e266b534248d.d
@@ -1,11 +1,12 @@
-/home/takaya/MyPrograming/CustomStorage/apps/backend/target/debug/deps/libdomain-49c4e266b534248d.rmeta: domain/src/lib.rs domain/src/user.rs domain/src/user/entity.rs domain/src/user/value_object.rs Cargo.toml
+/home/takaya/MyPrograming/CustomStorage/apps/backend/target/debug/deps/libdomain-49c4e266b534248d.rmeta: domain/src/lib.rs domain/src/models/mod.rs domain/src/models/user.rs domain/src/models/user/entity.rs domain/src/models/user/value_object.rs Cargo.toml
 
-/home/takaya/MyPrograming/CustomStorage/apps/backend/target/debug/deps/domain-49c4e266b534248d.d: domain/src/lib.rs domain/src/user.rs domain/src/user/entity.rs domain/src/user/value_object.rs Cargo.toml
+/home/takaya/MyPrograming/CustomStorage/apps/backend/target/debug/deps/domain-49c4e266b534248d.d: domain/src/lib.rs domain/src/models/mod.rs domain/src/models/user.rs domain/src/models/user/entity.rs domain/src/models/user/value_object.rs Cargo.toml
 
 domain/src/lib.rs:
-domain/src/user.rs:
-domain/src/user/entity.rs:
-domain/src/user/value_object.rs:
+domain/src/models/mod.rs:
+domain/src/models/user.rs:
+domain/src/models/user/entity.rs:
+domain/src/models/user/value_object.rs:
 Cargo.toml:
 
 # env-dep:CLIPPY_ARGS=-A__CLIPPY_HACKERY__clippy::needless_return__CLIPPY_HACKERY__
--- apps/backend/target/debug/deps/domain-9fb5e080c184adb6.d
+++ apps/backend/target/debug/deps/domain-9fb5e080c184adb6.d
@@ -1,11 +1,12 @@
-/home/takaya/MyPrograming/CustomStorage/apps/backend/target/debug/deps/libdomain-9fb5e080c184adb6.rmeta: domain/src/lib.rs domain/src/user.rs domain/src/user/entity.rs domain/src/user/value_object.rs Cargo.toml
+/home/takaya/MyPrograming/CustomStorage/apps/backend/target/debug/deps/libdomain-9fb5e080c184adb6.rmeta: domain/src/lib.rs domain/src/models/mod.rs domain/src/models/user.rs domain/src/models/user/entity.rs domain/src/models/user/value_object.rs Cargo.toml
 
-/home/takaya/MyPrograming/CustomStorage/apps/backend/target/debug/deps/domain-9fb5e080c184adb6.d: domain/src/lib.rs domain/src/user.rs domain/src/user/entity.rs domain/src/user/value_object.rs Cargo.toml
+/home/takaya/MyPrograming/CustomStorage/apps/backend/target/debug/deps/domain-9fb5e080c184adb6.d: domain/src/lib.rs domain/src/models/mod.rs domain/src/models/user.rs domain/src/models/user/entity.rs domain/src/models/user/value_object.rs Cargo.toml
 
 domain/src/lib.rs:
-domain/src/user.rs:
-domain/src/user/entity.rs:
-domain/src/user/value_object.rs:
+domain/src/models/mod.rs:
+domain/src/models/user.rs:
+domain/src/models/user/entity.rs:
+domain/src/models/user/value_object.rs:
 Cargo.toml:
 
 # env-dep:CLIPPY_ARGS=-A__CLIPPY_HACKERY__clippy::needless_return__CLIPPY_HACKERY__
