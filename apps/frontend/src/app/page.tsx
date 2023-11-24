ecddb9dc52552c4c26666359ca1f4abd5900582e
takaya <takaya_ishiyama0810@yahoo.co.jp>
2023-11-24 21:13:54 +0900

    front

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <></>;
+"use client";
+
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
+
+export default function MyApp() {
+	const router = useRouter();
+	const handleClickGoHomePage = () => {
+		router.push("/home");
+	};
+	return (
+		<>
+			<div>
+				<Button variant="contained" onClick={handleClickGoHomePage} />
+			</div>
+		</>
+	);
 }
