9920f333a359d0fba8e4a09a1fe04d6e6cd66b5e
takaya <takaya_ishiyama0810@yahoo.co.jp>
2024-01-02 23:22:16 +0900

    a

--- apps/backend/infrastructure/src/test/setup_testdb.rs
+++ apps/backend/infrastructure/src/test/setup_testdb.rs
@@ -1,12 +1,5 @@
-use sqlx::{PgPool, Pool, Postgres};
+use sqlx::PgPool;
 
 pub async fn setup_database(pool: &PgPool) {
-    // dotenv::dotenv().ok();
-    // let database_url = std::env::var("TEST_DATABASE_URL").expect("DATABASE_URL is not set");
-
-    // let pool = pool::Pool::<Postgres>::connect(&database_url)
-    //     .await
-    //     .unwrap();
-
     sqlx::migrate!("src/db/migrations").run(pool).await.unwrap();
 }
