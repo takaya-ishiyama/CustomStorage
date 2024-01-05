8680886c583fef66b28472e10e244ea0ff992a4b
takaya <takaya_ishiyama0810@yahoo.co.jp>
2024-01-05 23:39:05 +0900

    a

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -4,11 +4,25 @@ import React from "react";
 import { CustomHeader } from "../components/features/CustomHeader";
 import { ProviderWrapper } from "../hooks/Provider/ProviderWrapper";
 import { LoginFormContents } from "./components/LoginFormContents";
+import { Box } from "@mui/material";
+import { useAtomValue } from "jotai";
+import { userAtom } from "../hooks/jotai/user/atom";
+import { useRouter } from "next/navigation";
+import { Routes } from "../routes";
+import { Loading } from "../components/molecules/Loading";
 
 const Login = () => {
+	const user = useAtomValue(userAtom);
+	const router = useRouter();
+	React.useEffect(() => {
+		console.log("eeecccd", user);
+		if (user.id != null && user.id !== "") router.push(Routes.home);
+	}, [user, router]);
+	if (user === null || user === undefined) return <Loading />;
 	return (
 		<ProviderWrapper>
 			<CustomHeader />
+			<Box>ログイン</Box>
 			<LoginFormContents />
 		</ProviderWrapper>
 	);
--- apps/frontend/src/app/register/page.tsx
+++ apps/frontend/src/app/register/page.tsx
@@ -4,11 +4,13 @@ import React from "react";
 import { ProviderWrapper } from "../hooks/Provider/ProviderWrapper";
 import { CustomHeader } from "../components/features/CustomHeader";
 import { FormContents } from "./components/FormContents";
+import { Box } from "@mui/material";
 
 const Register = () => {
 	return (
 		<ProviderWrapper>
 			<CustomHeader />
+			<Box>ユーザー登録</Box>
 			<FormContents />
 		</ProviderWrapper>
 	);
